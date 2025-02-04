0c33765262ababcb4a89c581eb3ee7b913f7bd6b
fleroux <francois.leroux.pro@gmail.com>
2025-02-04 12:05:53 +0100

    opening fits file with astropy

--- neon.py
+++ neon.py
@@ -5,6 +5,22 @@ Created on Mon Feb  3 22:06:56 2025
 @author: fleroux
 """
 
+from pathlib import Path
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+from astropy.io import fits
+
+filename = "ngc4065_panstarrs_g.fits"
+
+path = Path(__file__).parent / filename
+
+#hdul = fits.open(path)
+
+with fits.open(path) as hdul:
+   hdul.verify('fix')
+   data = np.array(hdul[0].data)
+   
+plt.imshow(np.log(data), cmap='viridis')
+plt.show()
